fd1dd3694ca0f4b27adcb21d92b99722d91e488c
picoreclame <peter@picoreclame.nl>
2013-10-24 14:00:59 +0200

    Intial Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+$(window).load(function() {
+      $('#headerblock-logo').delay(1000).fadeIn(300);
+      $('.navigationcontainer').delay(1000).fadeIn(300);
+      $('.firstrow').delay(300).fadeIn(150);
+      $('.secondrow').delay(350).fadeIn(150);
+      $('.thirdrow').delay(400).fadeIn(150);
+      $('.footercontainer').delay(450).fadeIn(150);
+});
